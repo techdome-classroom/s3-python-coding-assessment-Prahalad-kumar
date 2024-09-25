bdb90169fc58ce0316c98fee96ae4507380d66b7
121deathboy <121deathboy@gmail.com>
2024-09-25 16:02:28 +0530

    Problem 2 solution wrote in program2.py

--- program2.py
+++ program2.py
@@ -4,7 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
-
-
-
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman_map[s[i]] < roman_map[s[i + 1]]:
+                result -= roman_map[s[i]]
+            else:
+                result += roman_map[s[i]]
+        
+        return result
